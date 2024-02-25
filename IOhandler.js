7411bc48605634cd7526e1a841134e2e68a306ff
skymiri <jyoon69@my.bcit.ca>
2024-02-24 16:04:59 -0800

    Lab

--- IOhandler.js
+++ IOhandler.js
@@ -1,5 +1,3 @@
-
-
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
@@ -13,52 +11,49 @@ const unzipper = require("unzipper"),
  * @return {promise}
  */
 const unzip = (pathIn, pathOut) => {
-  const yauzl = require('yauzl-promise');
-  
-  return yauzl.open(pathIn, { lazyEntries: true })
-    .then(zipfile => {
-      const ignoreDir = 'HiddenDir/';
-      const ignoreDirLength = ignoreDir.length;
-      
-      const extractEntry = entry => {
-        const entryPath = entry.fileName;
-        
-        if (entryPath.startsWith(ignoreDir)) {
-          return;
-        }
-        
-        const outputPath = path.join(pathOut, entryPath);
-        
-        if (/\/$/.test(entryPath)) {
-          // Create directory if entry is a directory
-          return fs.promises.mkdir(outputPath, { recursive: true });
-        } else {
-          // Extract file if entry is a file
-          return zipfile.openReadStream(entry)
-            .then(readStream => {
-              const writeStream = fs.createWriteStream(outputPath);
-              readStream.pipe(writeStream);
-              return new Promise((resolve, reject) => {
-                writeStream.on('finish', resolve);
-                writeStream.on('error', reject);
-              });
-            });
-        }
-      };
-      
-      const extractNextEntry = () => {
-        const entry = zipfile.readEntry();
-        
-        if (!entry) {
-          return zipfile.close();
-        }
-        
-        return extractEntry(entry)
-          .then(extractNextEntry);
-      };
-      
-      return extractNextEntry();
-    });
+  const yauzl = require("yauzl-promise");
+
+  return yauzl.open(pathIn, { lazyEntries: true }).then((zipfile) => {
+    const ignoreDir = "HiddenDir/";
+    const ignoreDirLength = ignoreDir.length;
+
+    const extractEntry = (entry) => {
+      const entryPath = entry.fileName;
+
+      if (entryPath.startsWith(ignoreDir)) {
+        return;
+      }
+
+      const outputPath = path.join(pathOut, entryPath);
+
+      if (/\/$/.test(entryPath)) {
+        // Create directory if entry is a directory
+        return fs.promises.mkdir(outputPath, { recursive: true });
+      } else {
+        // Extract file if entry is a file
+        return zipfile.openReadStream(entry).then((readStream) => {
+          const writeStream = fs.createWriteStream(outputPath);
+          readStream.pipe(writeStream);
+          return new Promise((resolve, reject) => {
+            writeStream.on("finish", resolve);
+            writeStream.on("error", reject);
+          });
+        });
+      }
+    };
+
+    const extractNextEntry = () => {
+      const entry = zipfile.readEntry();
+
+      if (!entry) {
+        return zipfile.close();
+      }
+
+      return extractEntry(entry).then(extractNextEntry);
+    };
+
+    return extractNextEntry();
+  });
 };
 
 /**
@@ -67,13 +62,19 @@ const unzip = (pathIn, pathOut) => {
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {
-  return new transform({
-    Objectmode: true,
-    function (chunk, encoding, callback) {
-    callback(null, chunk);
+async function readDir(dir) {
+  return new promise((resolve, reject) => {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        const pngFiles = files.filter(
+          (file) => path.extname(file).toLowerCase() === ".png"
+        );
+      }
+    });
   });
-};
+}
 
 /**
  * Description: Read in png file by given pathIn,
@@ -85,29 +86,32 @@ const readDir = (dir) => {
  */
 const grayScale = (pathIn, pathOut) => {
   var fs = require("fs"),
-  PNG = require("pngjs").PNG;
+    PNG = require("pngjs").PNG;
 
-  fs.createReadStream("in.png")
-  .pipe(
-    new PNG({
-      filterType: 4,
-    })
-  )
-  .on("parsed", function () {
-    for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
+  fs.createReadStream(pathIn)
+    .pipe(
+      new PNG({
+        filterType: 4,
+      })
+    )
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
 
-      // invert color
-        this.data[idx] = 255 - this.data[idx];
-        this.data[idx + 1] = 255 - this.data[idx + 1];
-        this.data[idx + 2] = 255 - this.data[idx + 2];
-
+          // invert color
+          let gray =
+            this.data[idx] * 0.21 +
+            this.data[idx + 1] * 0.72 +
+            this.data[idx + 2];
+          this.data[idx] = gray;
+          this.data[idx + 1] = gray;
+          this.data[idx + 2] = gray;
+        }
       }
-    }
 
-    this.pack().pipe(fs.createWriteStream("out.png"));
-  });
+      this.pack().pipe(fs.createWriteStream(pathOut));
+    });
 };
 
 module.exports = {
--- main.js
+++ main.js
@@ -10,13 +10,19 @@ const path = require("path");
  */
 
 const IOhandler = require("./IOhandler");
+const { grayScale } = require("../app");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
-
+const { unzipFile, readDir } = require("./IOhandler");
 async function main() {
-  await IOhandler.unzip();
-  const imgs = await IOhandler.readDir();
+  await unzipFile(zipFilePath);
 
-  IOhandler.unzip(zipFilePath, pathUnzipped).then(() => IOhandler.readDir());
+  const files = await readDir(pathUnzipped);
+
+  files.forEach((file) => {
+    grayScale(file, pathProcessed);
+  });
 }
+
+main();
